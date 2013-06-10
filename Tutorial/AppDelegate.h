0973a5aae49032c7be749b5aed5902d305e4330f
rgrzhao <rgrzhao@gmail.com>
2013-06-10 16:13:52 -0700

    Initial Commit

---
+++ Tutorial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tutorial
+//
+//  Created by Roger Zhao on 2013-06-10.
+//  Copyright (c) 2013 Roger Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tutorial/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tutorial
+//
+//  Created by Roger Zhao on 2013-06-10.
+//  Copyright (c) 2013 Roger Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
